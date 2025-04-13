78bc9b1ba5817627400ab291bfe49e6bc20d1c67
silvmike <silvanovich.michael@gmail.com>
2025-04-13 21:04:54 +0300

    added embeddings service

---
+++ embed/test.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from typing import List
+from sentence_transformers import SentenceTransformer
+import uvicorn
+
+app = FastAPI()
+model = SentenceTransformer('ai-forever/sbert_large_nlu_ru', device='cpu')
+
+@app.post("/embed")
+async def embed(texts: List[str]):
+    return model.encode(texts).tolist()
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
