58387ea7f4deb2788f1231a9ee5b83de12d95d7b
silvmike <silvanovich.michael@gmail.com>
2025-04-13 23:29:37 +0300

    fixed tests

--- controller/src/test/kotlin/com/example/demo/DemoApplicationTests.kt
+++ controller/src/test/kotlin/com/example/demo/DemoApplicationTests.kt
@@ -1,13 +1,17 @@
 package com.example.demo
 
+import com.example.demo.app.DemoApplication
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
 
 @SpringBootTest
+@ActiveProfiles("STUB")
+@ContextConfiguration(classes = [DemoApplication::class])
 class DemoApplicationTests {
 
 	@Test
 	fun contextLoads() {
 	}
-
 }
