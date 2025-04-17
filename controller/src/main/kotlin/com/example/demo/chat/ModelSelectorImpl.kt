490e94ad58d25aa9b59f1d740018a62c46abbe7f
silvmike <silvanovich.michael@gmail.com>
2025-04-17 22:02:06 +0300

    added logging

--- controller/src/main/kotlin/com/example/demo/chat/ModelSelectorImpl.kt
+++ controller/src/main/kotlin/com/example/demo/chat/ModelSelectorImpl.kt
@@ -2,12 +2,14 @@ package com.example.demo.chat
 
 import com.example.demo.chat.api.ModelSelector
 import com.example.demo.chat.giga.api.GigaChatClient
+import org.slf4j.LoggerFactory
 import java.util.concurrent.ConcurrentHashMap
 
 class ModelSelectorImpl(
     private val gigaChatClient: GigaChatClient
 ) : ModelSelector {
 
+    private val logger = LoggerFactory.getLogger(javaClass)
     private val modelCache = ConcurrentHashMap<String, String>()
 
     override fun selectModel(): String =
@@ -17,5 +19,8 @@ class ModelSelectorImpl(
                 .map { it.id }
                 .filterNotNull()
                 .first()
+                .apply {
+                    logger.info("[GigaChat] Selected model: $this")
+                }
         }
 }
